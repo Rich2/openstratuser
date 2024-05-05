9c1fa77e51297cc9a5414350d397964c3f5efdc0
Rich2 <rr1@tutanota.com>
2024-05-05 11:32:37 +0100

    Added unsafe.

--- UserApp.scala
+++ UserApp.scala
@@ -1,11 +1,15 @@
 /* Copyright 2024 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package osuser
-import ostrat._, geom._, prid.phex._
+import zio._, ostrat._, geom._, prid.phex._
 
 object UserApp extends App
 {
+  def iz1: Task[Int] = ZIO.succeed(5)
+
+  Unsafe.unsafe(implicit u => zio.Runtime.default.unsafe.run(iz1))
   val p1 = Pt2(5, 8)
   debvar(p1)
+
   val hex1 = HCen(4, 4)
   debvar(hex1)
 }
--- osuser.sbt
+++ osuser.sbt
@@ -1,7 +1,7 @@
 name := "OpenStratUser"
 version := "0.0.1snap"
 scalaVersion := "3.4.1"
-libraryDependencies += "dev.zio" %% "zio" % "2.1.0-RC3"
+libraryDependencies += "dev.zio" %% "zio" % "2.1.0-RC3" withSources() withJavadoc()
 resolvers += "RichStrat" at "https://richstrat.com/repository"
 libraryDependencies += "com.richstrat" % "rutil" % "0.3.2"
 libraryDependencies += "com.richstrat" % "geom" % "0.3.2"
