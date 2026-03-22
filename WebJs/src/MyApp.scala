eeae3b7da6596d48780b10c2fcf3981178a3e34d
Rich2 <rr1@tutanota.com>
2026-03-22 16:56:10 +0000

    Created WebJs App.

---
+++ WebJs/src/MyApp.scala
@@ -0,0 +1,11 @@
+/* Copyright 2026 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package webjs
+import ostrat.*
+
+object HelloApp
+{
+  def main(args: Array[String]): Unit =
+  {
+    deb("Hello World")
+  }
+}
